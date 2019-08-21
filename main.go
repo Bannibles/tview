0b60bcaf1caa76c26bff61a4015d01767774b562
Daniel Issoupov <daniel@deniss-mbp.sfdcgomobile.salesforce.com>
2019-08-21 11:21:02 -0700

    Dropdown menu placeholder

--- main.go
+++ main.go
@@ -1,12 +1,50 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/rivo/tview"
 )
 
+var appMenu = []string{
+	"1 - Demo",
+	//"2 - Blah...",
+	"0 - Exit",
+}
+
+type myapp struct {
+	app *tview.Application
+	// TODO: add more what you need here
+}
+
+func initApp() {
+	a := &myapp{
+		app: tview.NewApplication(),
+	}
+
+	//box := tview.NewBox().SetBorder(true).SetTitle("  Press CTRL-C to exit  ")
+	dropdown := tview.NewDropDown().
+		SetLabel("Select an option (hit Enter): ").
+		SetOptions(appMenu, a.onAppChoice)
+
+	a.app.SetRoot(dropdown, true)
+
+	if err := a.app.Run(); err != nil {
+		panic("FAILED TO RUN: " + err.Error())
+	}
+}
+
 func main() {
-	box := tview.NewBox().SetBorder(true).SetTitle("tview algos")
-	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
-		panic(err)
+	initApp()
+}
+
+func (a *myapp) onAppChoice(menu string, idx int) {
+	switch menu[:1] {
+	case "1":
+		// TODO: handle
+		fmt.Printf("Your choice is %s [%d]", menu, idx)
+	case "0":
+		a.app.Stop()
+		return
 	}
 }
